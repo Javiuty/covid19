224ea50e75674b82e9ddee1d5c9718678f26c489
Javiuty <javiiescuadra@gmail.com>
2020-09-16 18:18:12 +0200

    creating chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -8,26 +8,28 @@ const Chart = () => {
   const [dailyData, setDailyData] = useState({});
 
   useEffect(() => {
-    const fetchAPI = async () => {
-      setDailyData = await fetchDailyData();
+    const fetchMyAPI = async () => {
+      const initialDailyData = await fetchDailyData();
+
+      setDailyData(initialDailyData);
     };
 
-    fetchAPI();
+    fetchMyAPI();
   }, []);
 
-  const lineChart = dailyData[0] ? (
+  const lineChart = dailyData.length ? (
     <Line
       data={{
-        labels: dailyData(({ date }) => date),
+        labels: dailyData.map(({ date }) => date),
         datasets: [
           {
-            data: dailyData(({ confirmed }) => confirmed),
+            data: dailyData.map(({ confirmed }) => confirmed),
             label: "Infected",
             borderColor: "#3333ff",
             fill: true,
           },
           {
-            data: dailyData(({ deaths }) => deaths),
+            data: dailyData.map(({ deaths }) => deaths),
             label: "Deaths",
             borderColor: "red",
             backgroundColor: "rgba(255, 0, 0, 0.5)",
